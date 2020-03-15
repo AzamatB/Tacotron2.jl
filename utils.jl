8ce1b181027fb0f0dfe86132a541ad1bbb7b57b1
Azamat Berdyshev <aberdysh@gmail.com>
2020-03-15 15:44:57 +0600

    add constructor for Tacotron2 class

--- utils.jl
+++ utils.jl
@@ -1,13 +1,23 @@
-function Flux.dropout(x, p)
+function dropout(x, p)
    q = 1 - p
    y = rand!(similar(x))
    y .= Flux._dropout_kernel.(y, p, q)
    x .* y
 end
 
-Flux.@adjoint function Flux.dropout(x, p)
+@adjoint function dropout(x, p)
    q = 1 - p
    y = rand!(similar(x))
    y .= Flux._dropout_kernel.(y, p, q)
    return x .* y, Δ -> (Δ .* y, nothing)
+end
+
+@adjoint function reduce(::typeof(hcat), As::AbstractVector{<:AbstractVecOrMat})
+   cumsizes = cumsum(size.(As, 2))
+   return reduce(hcat, As), Δ -> (nothing, map((sz, A) -> Zygote.pull_block_horz(sz, Δ, A), cumsizes, As))
+end
+
+@adjoint function reduce(::typeof(vcat), As::AbstractVector{<:AbstractVecOrMat})
+   cumsizes = cumsum(size.(As, 1))
+   return reduce(vcat, As), Δ -> (nothing, map((sz, A) -> Zygote.pull_block_vert(sz, Δ, A), cumsizes, As))
 end
