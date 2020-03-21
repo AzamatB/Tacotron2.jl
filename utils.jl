9b84cfd9469f438eb0d77eb2cf53ea344e3f513e
azamat <aberdysh@gmail.com>
2020-03-21 10:54:07 +0600

    add Zygote.refresh() after adjoint definitions

--- utils.jl
+++ utils.jl
@@ -1,5 +1,5 @@
 using Zygote: @adjoint
-
+### helper functions for working with array types
 addparent(A::Type{<:AbstractArray}, P′::DataType = A) = A
 addparent(A::Type{CuArray{T,N,Nothing}}, P′::DataType = A) where {T,N} = CuArray{T,N,P′}
 addparent(A::Type{CuArray{T,N,P}}, P′::DataType) where {T,N,P} = CuArray{T,N,P′}
@@ -20,6 +20,7 @@ vectorof(::CuArray{T,N,P}) where {T,N,P} = CuVector{T,P}
 matrixof(::CuArray{T,N,P}) where {T,N,P} = CuMatrix{T,P}
 tensor₃of(::CuArray{T,N,P}) where {T,N,P} = CuArray{T,3,P}
 
+### Work around for github.com/FluxML/Flux.jl/issues/1084
 function Flux.dropout(x, p)
    q = 1 - p
    y = rand!(similar(x))
@@ -34,12 +35,13 @@ end
    return x .* y, Δ -> (Δ .* y, nothing)
 end
 
-@adjoint function reduce(::typeof(hcat), As::AbstractVector{<:AbstractVecOrMat})
+### Custom adjoint definitions
+@adjoint function Base.reduce(::typeof(hcat), As::AbstractVector{<:AbstractVecOrMat})
    cumsizes = cumsum(size.(As, 2))
    return reduce(hcat, As), Δ -> (nothing, map((sz, A) -> Zygote.pull_block_horz(sz, Δ, A), cumsizes, As))
 end
 
-@adjoint function reduce(::typeof(vcat), As::AbstractVector{<:AbstractVecOrMat})
+@adjoint function Base.reduce(::typeof(vcat), As::AbstractVector{<:AbstractVecOrMat})
    cumsizes = cumsum(size.(As, 1))
    return reduce(vcat, As), Δ -> (nothing, map((sz, A) -> Zygote.pull_block_vert(sz, Δ, A), cumsizes, As))
 end
@@ -49,7 +51,7 @@ veclength(x::AbstractVector{<:Tuple}) = length(x)
 pull_block_vert(idx, ȳ, x::Tuple) = ȳ[idx]
 pull_block_vert(lastidx, ȳ, x::AbstractVector{<:Tuple}) = ȳ[(lastidx-length(x)+1):lastidx]
 
-@adjoint function vcat(xs::Union{AbstractVector{T},T}...) where T <: Tuple
+@adjoint function Base.vcat(xs::Union{AbstractVector{T},T}...) where T <: Tuple
    lastidx = Ref(0)
    lastidxs = ntuple(length(xs)) do i
       @inbounds lastidx[] += veclength(xs[i])
@@ -61,7 +63,7 @@ end
 
 pull_block_horz(lastidx, ȳ, x::AbstractArray{<:Number,3}) = ȳ[:, (lastidx-size(x,2)+1):lastidx, :]
 
-@adjoint function hcat(xs::T₃...) where T₃ <: AbstractArray{<:Number,3}
+@adjoint function Base.hcat(xs::T₃...) where T₃ <: AbstractArray{<:Number,3}
    lastidx = Ref(0)
    lastidxs = ntuple(length(xs)) do i
       @inbounds lastidx[] += size(xs[i], 2)
@@ -70,3 +72,5 @@ pull_block_horz(lastidx, ȳ, x::AbstractArray{<:Number,3}) = ȳ[:, (lastidx-si
       (map((lastidx, x) -> pull_block_horz(lastidx, ȳ, x), lastidxs, xs)...,)
    end
 end
+
+Zygote.refresh()
